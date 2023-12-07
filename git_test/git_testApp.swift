97411c6f4c75b2bd6448fff768fd3edd50a18db0
Alexandru Anghel <alexandru@ditar.io>
2023-12-07 13:37:56 +0100

    Initial Commit

---
+++ git_test/git_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_testApp.swift
+//  git_test
+//
+//  Created by Alex Anghel on 07/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct git_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
